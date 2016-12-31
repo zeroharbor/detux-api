ac948d6559c72225da499db27c32b832b42bd854
deadbits <adam@deadbits.org>
2016-12-31 17:41:15 -0500

    add preamble to init

--- detux/__init__.py
+++ detux/__init__.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Author: deadbits
-# @Date:   2016-12-30 13:14:31
-# @Last Modified by:   deadbits
-# @Last Modified time: 2016-12-31 14:16:33
+# ========
+#    Python library to interact with the Detux.org API and an accompanying CLI application
+#    using the library for more straight forward user access.
+#
+#    Copyright (C) 2016  Adam M. Swanda (adam@zeroharbor.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ========
 
 from .detux import Detux
 from .detux import Store
