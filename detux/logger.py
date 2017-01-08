25066fc7e1127b67914b92cc6242e4b6c2ff40ce
deadbits <adam@deadbits.org>
2017-01-08 10:26:47 -0500

    stream logger for verbose output

---
+++ detux/logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ========
+#    Python library to interact with the Detux.org API and an accompanying CLI application
+#    using the library for more straight forward user access.
+#
+#    Copyright (C) 2017  Adam M. Swanda (adam@zeroharbor.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ========
+
+import logging
+import logging.handlers
+
+
+def setup_logger():
+    """ Initialize a stream logger for verbose messages """
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(module)s: %(message)s'))
+    handler.setLevel(logging.DEBUG)
+    logger = logging.getLogger('detux-api')
+    logger.addHandler(handler)
+    return logger
